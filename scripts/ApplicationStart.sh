b081ac670a6fab44d2c0908621dafaaf39a3a111
BobbyMulla <bobbyat13@gmail.com>
2025-08-02 20:53:31 +0530

    added deployment files

---
+++ scripts/ApplicationStart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Starting node server"
+
+cd /home/ec2-user/nodeapp
+
+nohup npm run start > app.log 2>&1 &
---
+++ scripts/BeforeInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Stopping node server if running..."
+pkill node || echo "No node process running"
