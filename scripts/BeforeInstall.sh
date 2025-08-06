0f47917869cd32ca342bfdf3488db1bdb0d861de
BobbyMulla <bobbyat13@gmail.com>
2025-08-06 18:30:42 +0530

    mkdir comment in before install

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -5,6 +5,10 @@ pkill node || echo "No node process running"
 
 # Inside scripts/BeforeInstall.sh
 
+echo "Making directory if not exists..."
+mkdir -p /home/ubuntu/nodeapp
+
+
 echo "Fixing file permissions..."
 sudo chown -R ubuntu:ubuntu /home/ubuntu/nodeapp
 
