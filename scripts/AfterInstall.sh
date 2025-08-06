0c109334b800c7dc4a0ded0696b0a337691e2173
BobbyMulla <bobbyat13@gmail.com>
2025-08-06 18:40:07 +0530

    shit

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Fixing file ownership..."
+sudo chown -R ubuntu:ubuntu /home/ubuntu/nodeapp
+sudo chmod -R u+rw /home/ubuntu/nodeapp
+
+echo "Installing dependencies..."
+cd /home/ubuntu/nodeapp
+npm install
+
+
 cd /home/ubuntu/nodeapp
 
 npm install
--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -3,9 +3,3 @@
 echo "Stopping node server if running..."
 pkill node || echo "No node process running"
 
-echo "Creating nodeapp directory if not exists..."
-mkdir -p /home/ubuntu/nodeapp
-
-echo "Fixing file permissions..."
-sudo chown -R ubuntu:ubuntu /home/ubuntu/nodeapp
-sudo chmod -R u+rw /home/ubuntu/nodeapp
