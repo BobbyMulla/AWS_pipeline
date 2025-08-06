aae256393c3c2ea339ff58585530d77267cea42f
BobbyMulla <bobbyat13@gmail.com>
2025-08-06 18:52:30 +0530

    sudo rm rf

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -3,3 +3,11 @@
 echo "Stopping node server if running..."
 pkill node || echo "No node process running"
 
+echo "Deleting previous app directory..."
+rm -rf /home/ubuntu/nodeapp
+
+echo "Creating fresh directory..."
+mkdir -p /home/ubuntu/nodeapp
+
+echo "Fixing permissions..."
+chown -R ubuntu:ubuntu /home/ubuntu/nodeapp
