6e99329ceb86ab8cf1031d163d5fac1ee0e650e9
BobbyMulla <bobbyat13@gmail.com>
2025-08-06 19:14:49 +0530

    permissions added

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -2,3 +2,7 @@
 
 echo "Stopping node server if running..."
 pkill node || echo "No node process running"
+
+echo "Fixing permissions..."
+chown -R ubuntu:ubuntu /home/ubuntu/nodeapp
+sudo chmod -R u+rw /home/ubuntu/nodeapp
