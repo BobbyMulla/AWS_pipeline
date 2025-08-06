a850c4799c00e5579d66723a276b82edf847dd07
BobbyMulla <bobbyat13@gmail.com>
2025-08-06 18:23:58 +0530

    modified beforeinstall

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -2,3 +2,9 @@
 
 echo "Stopping node server if running..."
 pkill node || echo "No node process running"
+
+# Inside scripts/BeforeInstall.sh
+
+echo "Fixing file permissions..."
+sudo chown -R ubuntu:ubuntu /home/ubuntu/nodeapp
+
