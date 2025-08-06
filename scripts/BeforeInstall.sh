b50a383fc18a9eee7e5f0e6d02812a8c014706e1
BobbyMulla <bobbyat13@gmail.com>
2025-08-06 18:35:31 +0530

    added chmod

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -3,12 +3,9 @@
 echo "Stopping node server if running..."
 pkill node || echo "No node process running"
 
-# Inside scripts/BeforeInstall.sh
-
-echo "Making directory if not exists..."
+echo "Creating nodeapp directory if not exists..."
 mkdir -p /home/ubuntu/nodeapp
-
 
 echo "Fixing file permissions..."
 sudo chown -R ubuntu:ubuntu /home/ubuntu/nodeapp
-
+sudo chmod -R u+rw /home/ubuntu/nodeapp
